a4c89ac1032a9c6380540306e4e0c1058e2788cc
Kyle Richardson <kylerichardson@users.noreply.github.com>
2015-06-11 13:42:58 -0400

    New ruby script to remove eml journal headers
    Also a couple minor usage updates to geoip
    python script

---
+++ split-eml.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+# Usage: ruby split-eml.rb <dir_of_eml_files> [output_dir]
+
+input_dir = ARGV.shift
+output_dir = ARGV.shift
+unless output_dir
+    output_dir = input_dir
+end
+
+eml_files = []
+Dir.foreach(input_dir) do |file_name|
+    if file_name !~ /^\./
+        eml_files << file_name
+    end
+end
+
+puts eml_files.inspect if $DEBUG
+
+eml_files.each do |file_name|
+    puts "Input File: #{input_dir}#{file_name}" if $DEBUG
+    lines = open("#{input_dir}#{file_name}").readlines
+    while true
+        puts lines[0] if $DEBUG
+        if lines[0] !~ /^>From/
+            lines.shift
+        else
+            lines.shift
+            break
+        end
+    end
+    out_file = File.new("#{output_dir}alt-#{file_name}", "w")
+    lines.each do |line|
+        out_file.write(line)
+    end
+    out_file.close
+end
+
